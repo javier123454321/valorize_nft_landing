c0e75dfd32440dc6d951b544b0ce545740416109
Javier Gonzalez <javier.e.glz@gmail.com>
2022-06-15 15:43:44 -0500

    create landing site

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+      vpurple: {
+        '100': '#E1D6F5',
+      }
+    }
+    },
+  },
+  plugins: [],
+}
