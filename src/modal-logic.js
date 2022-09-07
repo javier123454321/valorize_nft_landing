d82d107c5802d4acea7509b8c1977bfb8361f686
Javier Gonzalez <javier.e.glz@gmail.com>
2022-09-07 14:31:39 -0500

    Add info

--- src/modal-logic.js
+++ src/modal-logic.js
@@ -1,3 +1,4 @@
+"use strict";
 
 document.addEventListener('alpine:init', () => {
     Alpine.data('mintModal', () => ({
@@ -5,36 +6,48 @@ document.addEventListener('alpine:init', () => {
             {
                 title: 'Mycelia',
                 imageSrc: '/dist/img/product-mycelia.png',
-                amountToMint: 0,
+                amountToMint: 1,
                 price: 0.5,
-                available: true
+                available: true,
+                description: "Get 5 hours of consultation time with our team of tokenomics experts and we will help you define a token's parameters, needs, KPIs, as well as next steps."
             },
             {
                 title: 'Silver',
                 imageSrc: '/dist/img/product-silver.png',
-                amountToMint: 0,
-                price: 0.3,
-                available: true
+                amountToMint: 1,
+                price: 0.1,
+                available: true,
+                description: "Token launch in our platform, today (normal price of 0.3 ETH) and 1 year access to our platform."
             },
             {
-                title: 'Gold',
-                imageSrc: '/dist/img/product-gold.png',
+                title: 'Diamond',
+                imageSrc: '/dist/img/product-diamond.png',
                 amountToMint: 0,
                 price: 0.3,
-                available: false
+                available: false,
+                description: "Conversation with tokenomics expert to define token, access to dashboard for a 2 year minimum and priority customer support."
             },
 
         ],
         index: 0,
+        showDescription: false,
         next() {
-            this.index++;
+            this.index = ++this.index % this.nfts.length;
         },
         previous() {
-            this.index--;
+            this.index = Math.abs(--this.index % this.nfts.length);
         },
         nft() {
-            console.log(this.index)
             return this.nfts[this.index]
+        },
+        mintLess() {
+            this.nfts[this.index].amountToMint > 1 && this.nfts[this.index].amountToMint--
+        },
+        mintMore() {
+            this.nfts[this.index].amountToMint < 10 && this.nfts[this.index].amountToMint++
+        },
+        price() {
+            return (this.nfts[this.index].price * this.nfts[this.index].amountToMint).toPrecision(2)
         }
     }))
 })
