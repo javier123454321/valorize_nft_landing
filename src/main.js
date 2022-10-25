13f906653675d37c2f69b2c2a4a89ee793987447
Javier Gonzalez <javier.e.glz@gmail.com>
2022-10-25 13:06:03 -0500

    add alpine

--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
 const animation = require('./animation')
-const Alpine = require('alpinejs')
 const { mintModalProductNft } = require('./modal-logic')
 const { artistsData } = require('./artists')
 const { mintMembership } = require('./minting-membership')
 
-console.log(Alpine)
-Window.Alpine = Alpine.default
 Window.Alpine.data('mintModal', mintModalProductNft)
 Window.Alpine.data('artistsData', artistsData)
 Window.Alpine.data('mintMembership', mintMembership)
